b1fa43995bf8da797c1cfc1bd55c494a8f1a00c3
Sirio Jansen-Sanchez <siriojansen@hotmail.com>
2024-05-02 18:56:38 -0400

    Added single callback reception

--- cyphal_motor_py/cyphal_motor.py
+++ cyphal_motor_py/cyphal_motor.py
@@ -16,7 +16,7 @@ import zubax.service.actuator
 
 
 class CyphalMotor():
-    def __init__(self, esc_id: int) -> None:
+    def __init__(self, status_id: int) -> None:
         node_info = uavcan.node.GetInfo_1.Response(
             software_version=uavcan.node.Version_1(major=0, minor=1),
             name="org.amra.cyphal_motor_py.cyphal_motor",
@@ -24,15 +24,15 @@ class CyphalMotor():
         self._node = pycyphal.application.make_node(node_info)
         
         # Create Subscribers
-        self.status_sub = self._node.make_subscriber(zubax.service.actuator.Status_1)
-        self.status_sub.receive_in_background(self.status_cb)
+        self.status_sub = self._node.make_subscriber(zubax.service.actuator.Status_1, status_id)
 
         self._node.start()
 
     async def run(self):
-        async for msg, _ in self.heartbeat_sub:
-         print(f"RUN Got Message State: {msg.value}")
-
+        msg, transfer_data = self.status_sub.receive(25)
+        print(f"RUN Got Message State: {msg.value}")
+        
+        
 
     async def status_cb(self, msg: uavcan.node.Heartbeat_1, transport_data: pycyphal.transport.TransferFrom) -> None:
         print(f"Got Message State: {msg.value}")
@@ -42,7 +42,7 @@ class CyphalMotor():
         self._node.close()
 
 async def main():
-    esc = CyphalMotor(5600)
+    esc = CyphalMotor(65)
     try:
         await esc.run()
     except KeyboardInterrupt:
