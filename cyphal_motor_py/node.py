77b586487535853c41e753271993c23d802f0e5b
Sirio Jansen-Sanchez <siriojansen@hotmail.com>
2024-08-07 19:04:56 -0400

    Added node

--- cyphal_motor_py/node.py
+++ cyphal_motor_py/node.py
@@ -4,36 +4,39 @@ from cyphal_motor_py.cyphal_motor import CyphalMotor
 
 from std_msgs.msg import Float32
 
+import asyncio
+
 class CyphalMotorNode(Node):
     def __init__(self):
         super().__init__("motor")
         self.motor = CyphalMotor(self.motor_id, self.status_id, self.rat_setpoint_id, self.readiness_id)
-        self.timer = self.create_timer(self.deadman_timeout, self.cb_timer)
 
         # Declare Subscriber
         self.sub_setpoint = self.create_subscription(Float32, "setpoint", self.cb_setpoint)
-        self.setpoint = 0
+        self.setpoint = 0.0
+
+        self.timer = self.create_timer(self.deadman_timeout, self.cb_timer)
 
     def cb_setpoint(self, msg):
         self.setpoint = msg.data
-        pass
+        
     
     def cb_arm(self, msg):
         # enable timer 
         self.timer.reset()
         # send arm readiness command - 3
-        self.motor.publish_readiness(3)
-        pass
+        asyncio.run( self.motor.publish_readiness(3) )
+        
 
     def cb_disarm(self, msg):
         # disable timer
         self.timer.cancel()
         # send disarm readiness command - 2
-        self.motor.publish_readiness(2)
-        pass
+        asyncio.run( self.motor.publish_readiness(2) )
+        
     
     def cb_timer(self):
-        self.motor.publish_setpoint(self.setpoint, 0)
+        asyncio.run( self.motor.publish_setpoint(self.setpoint, 0) )
 
     def _get_parameters(self):
         # Declare Parameters
@@ -41,7 +44,7 @@ class CyphalMotorNode(Node):
         self.declare_parameter("status_id", 1200)
         self.declare_parameter("rat_setpoint_id", 1100)
         self.declare_parameter("readiness_id", 0)
-        self.declare_parameter("deadman_timeout", 0.35)
+        self.declare_parameter("deadman_timeout", 0.5)
 
         # Get Parameters
         self.motor_id        =  self.get_parameter("motor_id")
